a8412b878bc86655fe6e6de54a7329657f5663ec
vansh25090010 <vanshbansalcubastion@gmail.com>
2026-01-19 11:45:39 +0530

    Update main.tf

--- terraform-azure-vm/main.tf
+++ terraform-azure-vm/main.tf
@@ -63,11 +63,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
-# Storage Account
-resource "azurerm_storage_account" "storage" {
-  name                     = "tfstorageacct123"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+
